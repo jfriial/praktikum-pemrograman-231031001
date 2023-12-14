3d75f9134a71cb4de568f1067a16d48177d0ae79
jfriial <147455815+jfriial@users.noreply.github.com>
2023-12-14 10:48:18 +0800

    upload praktikum a1

---
+++ praktikum-1.py
@@ -0,0 +1,78 @@
+print('praktikum-1')
+print ('')
+#Print Nama Lengkap, Nim, Prodi
+print ('Nama Lengkap : Jefri Al Buchari')
+print ('Nim          : 231031001')
+print ('Prodi        : Sistem Informasi')
+
+x = 2023
+print('')
+print ('Angkatan', x)
+('')
+
+print()
+
+a = 10
+print ('data adalah:', a)
+print ('Tipe data a adalah', type(a))
+print()
+
+b = 10.0
+print ('data adalah:', b)
+print ('Tipe data b adalah', type(b))
+print()
+
+kampus = ('Institut Teknologi Bj Habibie')
+print('tipe data kampus adalah', kampus)
+print('tipe data adalah', type(kampus))
+
+print()
+
+c = complex(2,3)
+print('data adalah:', c)
+print('tipe data adalah:', type(c))
+print()
+
+log = True
+print('data adalah:', log)
+print('tipe data adalah:', type(log))
+print()
+
+p = 20
+q = 23
+hasil = p + q
+print('hasil', p, '+',q, '=',hasil)
+print('ini adalah batas untuk tugas praktikum###')
+
+print()
+
+#Operasi Aritmatika Dasar
+print("PENGURANGAN")
+p=20
+q=23
+hasil = p-q
+print('Hasil',p,"-",q,'=',hasil)
+print()
+print("PERKALIAN")
+hasil = p*q
+print("Hasil",p,"x",q,"=",hasil)
+print()
+print("PEMBAGIAN")
+k=20
+c=5
+hasil = k/c
+print("Hasil",k,"/",c,"=",hasil)
+print()
+print("MODULUS")
+hasil = q%c
+print("Hasil",q,"%",c,"=",hasil)
+print()
+print("PEMBAGIAN BULAT/Floor division")
+hasil = q//c
+print("Hasil",q,"//",c,"=",hasil)
+print()
+print("PANGKAT/Eksponensial")
+r=2
+hasil = c ** r
+print("Hasil",c,"**",r,"=",hasil)
+print("TUGAS SELESAI")
